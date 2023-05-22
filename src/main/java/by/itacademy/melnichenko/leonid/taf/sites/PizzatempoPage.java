1f9dcb7db43c27e6b44913d1647d67f225000267
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-22 17:17:13 +0300

    add pizzatempopage class

---
+++ src/main/java/by/itacademy/melnichenko/leonid/taf/sites/PizzatempoPage.java
@@ -0,0 +1,24 @@
+package by.itacademy.melnichenko.leonid.taf.sites;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PizzatempoPage {
+    WebDriver webDriver;
+    String inputEmailXPath = "/html/body/div/div[1]/form/p[1]/input";
+    String inputPasswordXPath = "/html/body/div/div[1]/form/p[2]/input[1]";
+    String buttonFindSudmit = "/html/body/div/div[1]/form/p[2]/input[2]";
+
+    public PizzatempoPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+    public void setInputEmail(String email){
+        webDriver.findElement(By.xpath(inputEmailXPath)).sendKeys(email);
+    }
+    public void setInputPassword(String password){
+        webDriver.findElement(By.xpath(inputPasswordXPath)).sendKeys(password);
+    }
+    public void setButtonFindSudmitClick(){
+        webDriver.findElement(By.xpath(buttonFindSudmit)).click();
+    }
+}
