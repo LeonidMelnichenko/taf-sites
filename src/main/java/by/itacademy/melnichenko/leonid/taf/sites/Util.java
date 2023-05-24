a1f8802b55afa3eba6701a30c4c24e0694193dfc
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-24 15:43:53 +0300

    create class Util

---
+++ src/main/java/by/itacademy/melnichenko/leonid/taf/sites/Util.java
@@ -0,0 +1,24 @@
+package by.itacademy.melnichenko.leonid.taf.sites;
+
+public class Util {
+    public static String generateEmail(){
+        String[] letter = {"a", "b", "с", "d", "e", "f", "g", "h"};
+        String result = "";
+        for(int i = 0; i < 8; i++){
+            int j;
+            j = (int) Math.random() * letter.length;
+            result += result.concat(letter[j]);
+        }
+        return result + "@gmail.com";
+    }
+    public static String generatePassword(){
+        String[] letter = {"1", "b", "2", "d", "3", "f", "4", "h"};
+        String result = "";
+        for(int i = 0; i < 8; i++){
+            int j;
+            j = (int) Math.random() * letter.length;
+            result += result.concat(letter[j]);
+        }
+        return result + "@gmail.com";
+    }
+}
