c2b60555e3a078e83f99853de0310a8706d87d56
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-26 16:05:52 +0300

    add refactoring PizzatempoPage

--- src/main/java/by/itacademy/melnichenko/leonid/taf/sites/PizzatempoPage.java
+++ src/main/java/by/itacademy/melnichenko/leonid/taf/sites/PizzatempoPage.java
@@ -5,20 +5,23 @@ import org.openqa.selenium.WebDriver;
 
 public class PizzatempoPage {
     private WebDriver webDriver;
-    private String inputEmailXPath = "/html/body/div/div[1]/form/p[1]/input";
-    private String inputPasswordXPath = "/html/body/div/div[1]/form/p[2]/input[1]";
-    private String buttonFindSudmit = "/html/body/div/div[1]/form/p[2]/input[2]";
+    private String inputEmailName = "astroauth_login";
+    private String inputPasswordName = "astroauth_pass";
+    private String buttonFindSudmitName = "astroauth_submit";
 
     public PizzatempoPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
     public void setInputEmail(String email){
-        webDriver.findElement(By.xpath(inputEmailXPath)).sendKeys(email);
+        webDriver.findElement(By.name(inputEmailName)).sendKeys(email);
     }
     public void setInputPassword(String password){
-        webDriver.findElement(By.xpath(inputPasswordXPath)).sendKeys(password);
+        webDriver.findElement(By.name(inputPasswordName)).sendKeys(password);
     }
     public void clickSetButtonFindSubmit(){
-        webDriver.findElement(By.xpath(buttonFindSudmit)).click();
+        webDriver.findElement(By.name(buttonFindSudmitName)).click();
+    }
+    public void getUrl(){
+        webDriver.get("https://www.pizzatempo.by/");
     }
 }
