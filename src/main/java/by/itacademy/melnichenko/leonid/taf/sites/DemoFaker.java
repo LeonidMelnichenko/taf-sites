0def4e316e10b05ef3aee13da1453d367cb89b4e
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-26 15:56:14 +0300

    create demofaker class

---
+++ src/main/java/by/itacademy/melnichenko/leonid/taf/sites/DemoFaker.java
@@ -0,0 +1,19 @@
+package by.itacademy.melnichenko.leonid.taf.sites;
+import com.github.javafaker.Faker;
+public class DemoFaker {
+    private Faker faker;
+
+    public DemoFaker(Faker faker) {
+        this.faker = faker;
+    }
+    public String generateFakerEmail(){
+        faker = new Faker();
+        String emailFaker = faker.internet().emailAddress();
+        return emailFaker;
+    }
+    public String generateFakerPassword(){
+        faker = new Faker();
+        String passwordFaker = faker.internet().password();
+        return passwordFaker;
+    }
+}
