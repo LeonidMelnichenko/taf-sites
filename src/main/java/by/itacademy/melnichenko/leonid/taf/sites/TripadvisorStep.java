fc3a6ec946f58a1e47c0457fe5a01a562a4f59a4
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-29 12:53:59 +0300

    add TripadvisorStep class

---
+++ src/main/java/by/itacademy/melnichenko/leonid/taf/sites/TripadvisorStep.java
@@ -0,0 +1,27 @@
+package by.itacademy.melnichenko.leonid.taf.sites;
+
+import org.openqa.selenium.WebDriver;
+
+public class TripadvisorStep {
+
+    TripadvisorPage tripadvisorPage;
+
+    public TripadvisorStep(WebDriver driver) {
+        this.tripadvisorPage = new TripadvisorPage(driver);
+    }
+    public void fillLoginFormAndSubmit(String emal, String password){
+        tripadvisorPage.clickSignInButton();
+        tripadvisorPage.switchToIframe();
+        tripadvisorPage.clickButtonContinueWithEmail();
+        tripadvisorPage.inputEmailAddressTo(emal);
+        tripadvisorPage.inputPasswordTo(password);
+        tripadvisorPage.buttonSignInApplyTo();
+    }
+    public void fillLoginFormAndSubmit(String emal){
+        tripadvisorPage.clickSignInButton();
+        tripadvisorPage.switchToIframe();
+        tripadvisorPage.clickButtonContinueWithEmail();
+        tripadvisorPage.inputEmailAddressTo(emal);
+        tripadvisorPage.buttonSignInApplyTo();
+    }
+}
